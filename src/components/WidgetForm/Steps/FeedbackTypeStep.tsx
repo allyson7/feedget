e7cb776f65510d21d4ec8143713b4cf278ff11a1
allyson7 <allysonlopes0@gmail.com>
2022-05-13 17:02:17 -0400

    : Close #2 Finished stage 2

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,13 +1,13 @@
-import { FeedBackType, feedBackTypes } from "..";
+import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 
-interface FeedBackTypeStepProps {
-  onFeedBackTypeChanged: (type: FeedBackType) => void;
+interface FeedbackTypeStepProps {
+  onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
 
-export function FeedBackTypeStep({
-  onFeedBackTypeChanged,
-}: FeedBackTypeStepProps) {
+export function FeedbackTypeStep({
+  onFeedbackTypeChanged,
+}: FeedbackTypeStepProps) {
   return (
     <>
       <header>
@@ -15,12 +15,12 @@ export function FeedBackTypeStep({
         <CloseButton />
       </header>
       <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedBackTypes).map(([key, value]) => {
+        {Object.entries(feedbackTypes).map(([key, value]) => {
           return (
             <button
               key={key}
               className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none transition"
-              onClick={() => onFeedBackTypeChanged(key as FeedBackType)}
+              onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
               type="button"
             >
               <img src={value.image.source} alt={value.image.alt} />
