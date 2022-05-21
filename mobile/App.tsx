3d0d5b5b6a295417965081adedbf07fb719cbae3
allyson7 <allysonlopes0@gmail.com>
2022-05-21 09:40:22 -0400

    #4: Expo google fonts configured successfully

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,10 +2,28 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { theme } from "./src/theme";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import * as SplashScreen from "expo-splash-screen";
 
 import { Widget } from "./src/components/Widget";
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
     <View
       style={{
